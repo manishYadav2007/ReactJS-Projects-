c991d8127b0db38e3b0df2c93f7e223185b54b56
= <=>
2025-12-15 15:02:31 +0530

    Modify FAQ App, Replace Class Component with Function Based Component and Hooks

--- Faqs App/src/components/FaqItem/index.jsx
+++ Faqs App/src/components/FaqItem/index.jsx
@@ -1,86 +1,38 @@
-// import "./index.css";
-// import { useState } from "react";
-// const FaqItem = (props) => {
-//   const { faqData } = props;
-//   const { id, questionText, answerText } = faqData;
-//   const [isClicked, setIsClicked] = useState(false);
-
-//   const onClickToggleFaqSection = () => {
-//     setIsClicked(!isClicked);
-//   };
-
-//   const plusIcon =
-//     "https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png";
-//   const minusIcon =
-//     "https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png";
-
-//   return (
-//     <li>
-//       <div className="faq-list-container">
-//         <p className="question">{questionText}</p>
-//         <button onClick={onClickToggleFaqSection}>
-//           <img
-//             src={isClicked ? minusIcon : plusIcon}
-//             className="icon-img"
-//             alt={isClicked ? "minus" : "plus"}
-//           />
-//         </button>
-//       </div>
-//       {isClicked ? (
-//         <div className="answer-container">
-//           <p className="answer-text">{answerText}</p>
-//         </div>
-//       ) : null}
-//     </li>
-//   );
-// };
-
-// export default FaqItem;
-
-import { Component } from "react";
 import "./index.css";
+import { useState } from "react";
+const FaqItem = (props) => {
+  const { faqData } = props;
+  const { id, questionText, answerText } = faqData;
+  const [isClicked, setIsClicked] = useState(false);
 
-class FaqItem extends Component {
-  state = {
-    isClicked: false,
+  const onClickToggleFaqSection = () => {
+    setIsClicked(!isClicked);
   };
 
-  onClickToggleFaqSection = () => {
-    this.setState((prevState) => ({
-      isClicked: !prevState.isClicked,
-    }));
-  };
+  const plusIcon =
+    "https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png";
+  const minusIcon =
+    "https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png";
 
-  render() {
-    const { isClicked } = this.state;
-    const { faqData } = this.props;
-    const { id, questionText, answerText } = faqData;
-
-    const minusIcon =
-      "https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png";
-    const plusIcon =
-      "https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png";
-
-    return (
-      <li>
-        <div className="faq-list-container">
-          <h1 className="question">{questionText}</h1>
-          <button onClick={this.onClickToggleFaqSection}>
-            <img
-              src={isClicked ? minusIcon : plusIcon}
-              className="icon-img"
-              alt={isClicked ? "minus" : "plus"}
-            />
-          </button>
+  return (
+    <li>
+      <div className="faq-list-container">
+        <p className="question">{questionText}</p>
+        <button onClick={onClickToggleFaqSection}>
+          <img
+            src={isClicked ? minusIcon : plusIcon}
+            className="icon-img"
+            alt={isClicked ? "minus" : "plus"}
+          />
+        </button>
+      </div>
+      {isClicked ? (
+        <div className="answer-container">
+          <p className="answer-text">{answerText}</p>
         </div>
-        {isClicked ? (
-          <div className="answer-container">
-            <p className="answer-text">{answerText}</p>
-          </div>
-        ) : null}
-      </li>
-    );
-  }
-}
+      ) : null}
+    </li>
+  );
+};
 
 export default FaqItem;
