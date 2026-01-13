2659c69ff60eb86e2159e52e5b287c06f614da0f
= <=>
2026-01-13 13:45:58 +0530

    POST: Create a New Project Which is IPL Dashbaord, Use React Router Home Page with IPL matches Detail Page

--- IPL Dashboard App/src/components/Home/index.jsx
+++ IPL Dashboard App/src/components/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { TailSpin } from "react-loader-spinner";
 import "./index.css";
